2e4ec44750a359fde1765c41c62492d2fca2881b
knowwen <337235669@qq.com>
2025-11-16 18:33:27 +0800

    badgateway oauth2-client

--- hello-vue/src/views/Login.vue
+++ hello-vue/src/views/Login.vue
@@ -6,12 +6,61 @@
 
     import api from '../api/api'
 
+    import {OAuth2Client } from '@badgateway/oauth2-client' // 引入 oauth2-client 库
+
     const router = useRouter()         // 获取路由实例
     const username = ref('')
     const password = ref('')
     const loading = ref(false)
+
+    const client = new OAuth2Client({
+      clientId: 'gateway',
+      server:'http://127.0.0.1:8080',
+      authorizationEndpoint: '/oauth2/authorize',
+      clientSecret:'secret',
+      tokenEndpoint: '/oauth2/token',
+      discoveryEndpoint: '/.well-known/',
+    });
+
+    function generateCodeVerifier() {
+      const array = new Uint32Array(56 / 2);
+      window.crypto.getRandomValues(array);
+      return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('');
+    }
+
     function oauth2_login(){
-      window.location.href = "http://127.0.0.1/oauth2/authorization/gateway";
+      const codeVerifier = async () => generateCodeVerifier();
+      const url = async () => 
+      client.authorizationCode.getAuthorizeUri({
+        redirectUri: 'http://localhost:5173/',
+        state: '12345',
+        codeVerifier,
+        scope: ['openid','profile','user'],
+      });
+      const oauth2Token = async () => { client.authorizationCode.getTokenFromCodeRedirect(
+        url,
+        {
+          /**
+           * The redirect URI is not actually used for any redirects, but MUST be the
+           * same as what you passed earlier to "authorizationCode"
+           */
+          redirectUri: 'http://localhost:5173/',
+          /**
+           * This is optional, but if it's passed then it also MUST be the same as
+           * what you passed in the first step.
+           *
+           * If set, it will verify that the server sent the exact same state back.
+           */
+          state: 'some-string',
+          codeVerifier,
+        }
+      )};
+      // .then((uri) => {
+      //   console.log('重定向到授权URI:', uri);
+      //   window.location.href = uri; // 重定向用户到授权URI
+      // }).then((error) => {
+      //   console.error('获取授权URI时出错:', error);
+      // });
     } 
     const login = async () => {
         loading.value = true 
